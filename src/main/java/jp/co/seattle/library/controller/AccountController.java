150fe8ff63192383b2fe08f0b5c9e4e66500d319
河上達志 <tatsushi@kawakamitatsusinoMacBook-ea.local>
2022-04-15 14:54:19 +0900

    再提出用

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -19,64 +19,62 @@ import jp.co.seattle.library.service.UsersService;
 /**
  * アカウント作成コントローラー
  */
-@Controller //APIの入り口
+@Controller // APIの入り口
 public class AccountController {
-    final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+	final static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @Autowired
-    private BooksService booksService;
-    @Autowired
-    private UsersService usersService;
+	@Autowired
+	private BooksService booksService;
+	@Autowired
+	private UsersService usersService;
 
-    @RequestMapping(value = "/newAccount", method = RequestMethod.GET) //value＝actionで指定したパラメータ
-    public String createAccount(Model model) {
-        return "createAccount";
-    }
+	@RequestMapping(value = "/newAccount", method = RequestMethod.GET) // value＝actionで指定したパラメータ
+	public String createAccount(Model model) {
+		return "createAccount";
+	}
 
-    /**
-     * 新規アカウント作成
-     *
-     * @param email メールアドレス
-     * @param password パスワード
-     * @param passwordForCheck 確認用パスワード
-     * @param model
-     * @return　ホーム画面に遷移
-     */
-    
-    @Transactional
-    @RequestMapping(value = "/createAccount", method = RequestMethod.POST)
-    public String createAccount(Locale locale,
-            @RequestParam("email") String email,
-            @RequestParam("password") String password,
-            @RequestParam("passwordForCheck") String passwordForCheck,
-            Model model) {
-        // デバッグ用ログ
-        logger.info("Welcome createAccount! The client locale is {}.", locale);
+	/**
+	 * 新規アカウント作成
+	 *
+	 * @param email            メールアドレス
+	 * @param password         パスワード
+	 * @param passwordForCheck 確認用パスワード
+	 * @param model
+	 * @return ホーム画面に遷移
+	 */
 
-        // パラメータで受け取った書籍情報をDtoに格納する。
-        UserInfo userInfo = new UserInfo();
-        userInfo.setEmail(email);
+	@Transactional
+	@RequestMapping(value = "/createAccount", method = RequestMethod.POST)
+	public String createAccount(Locale locale, @RequestParam("email") String email,
+			@RequestParam("password") String password, @RequestParam("passwordForCheck") String passwordForCheck,
+			Model model) {
+		// デバッグ用ログ
+		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
-        // TODO バリデーションチェック、パスワード一致チェック実装
-        if(password.length() >= 8 && password.matches("^[0-9a-zA-Z]+$")) {
-        	
-        	if(password.equals(passwordForCheck)) {
-        		userInfo.setPassword(password);
-        		usersService.registUser(userInfo);
-        		
-        		model.addAttribute("bookList",booksService.getBookList());
-        		return "login";        	
-            }else {
-        	model.addAttribute("errorPassword","パスワードが一致しません");
-        	return "createAccount";
-            }
-        	
-       }else {
-    	model.addAttribute("errorPassword","パスワードは8文字以上かつ半角英数字で入力してください。");
-    	return "createAccount";
-         	
-         }
-      
-    }
+		// パラメータで受け取った書籍情報をDtoに格納する。
+		UserInfo userInfo = new UserInfo();
+		userInfo.setEmail(email);
+
+		// TODO バリデーションチェック、パスワード一致チェック実装
+		if (password.length() >= 8 && password.matches("^[0-9a-zA-Z]+$")) {
+
+			if (password.equals(passwordForCheck)) {
+				userInfo.setPassword(password);
+
+				usersService.registUser(userInfo);
+
+				return "login";
+			} else {
+				model.addAttribute("errorPassword", "パスワードが一致しません");
+				return "createAccount";
+			}
+
+		} else {
+			model.addAttribute("errorPassword", "パスワードは8文字以上かつ半角英数字で入力してください。");
+			return "createAccount";
+
+		}
+
+	}
 
 }
